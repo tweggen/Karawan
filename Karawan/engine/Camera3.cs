10cec9678675a2b4921ae0f6305193b8421178c3
Timo Weggen <timo.weggen@gmail.com>
2023-01-14 13:36:47 +0100

    - added more data structures

---
+++ Karawan/engine/Camera3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karawan.engine
+{
+    class Camera3
+    {
+        public float Angle;
+        public float nearFrustrum;
+        public float farFrustrum;
+    }
+}
---
+++ Karawan/engine/Object3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+namespace Karawan.engine
+{
+    struct Object3
+    {
+        public UInt64 Parent;
+        public bool IsVisible;
+        public Quaternion Rotation;
+        public Vector3 Position;
+    }
+}
