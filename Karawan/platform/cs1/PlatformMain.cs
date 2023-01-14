6ad169e84aa0fe01a68fe6c85d8de0b8e7dcaa30
Timo Weggen <timo.weggen@gmail.com>
2023-01-14 12:12:50 +0100

    - added a new class.

---
+++ Karawan/platform/cs1/PlatformMain.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_CsLo;
+
+namespace Karawan.platform.cs1
+{
+    class PlatformMain
+    {
+        public static void Execute(string[] args)
+        {
+            Raylib.InitWindow(1280, 720, "Hello, Raylib-CsLo");
+            Raylib.SetTargetFPS(60);
+            // Main game loop
+            while (!Raylib.WindowShouldClose()) // Detect window close button or ESC key
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Raylib.SKYBLUE);
+                Raylib.DrawFPS(10, 10);
+                Raylib.DrawText("Raylib is easy!!!", 640, 360, 50, Raylib.RED);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
