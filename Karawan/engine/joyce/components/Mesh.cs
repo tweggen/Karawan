60aa6dacfb5a8fea9adc43ea3a9ca94e0a33245b
Timo Weggen <timo.weggen@gmail.com>
2023-01-20 09:58:36 +0000

    - added creation of normals.

--- Karawan/engine/joyce/components/Mesh.cs
+++ Karawan/engine/joyce/components/Mesh.cs
@@ -4,19 +4,98 @@ using System.Collections;
 
 namespace Karawan.engine.joyce.components
 {
-    struct Mesh
+    public struct Mesh
     {
         // TXWTODO: Come up with a supersmart concept only storing the mesh source/factory
         // TXWTODO: Let it use the IList interface
         public IList Vertices;
         public IList Indices;
         public IList UVs;
+        public IList Normals;
+
+        /**
+         * Generate smoothed normals for this mesh.
+         */
+        public void GenerateNormals()
+        {
+            if( null != Normals )
+            {
+                // TXWTODO: Throw
+                return;
+            }
+            var nVertices = Vertices.Count;
+            /*
+             * These are the normals we create. In the beginning,
+             * all of the normals are null.
+             */
+            Normals = new Vector3[nVertices];
+
+            /*
+             * We most likely sum more than one normal.
+             * Store the count of normal vectors we added.
+             * 
+             * TXWTODO: Don't we want to normalize them anyway?
+             */
+            var normalCount = new int[Vertices.Count];
+
+            var nIndices= Indices.Count;
+            /*
+             * 
+             */
+            for( int i=0; i<nIndices; i+=3 )
+            {
+                /*
+                 * Let's assume clockwise triangles.
+                 */
+                var v0 = (Vector3)Vertices[(int)Indices[i + 0]];
+                var v1 = (Vector3)Vertices[(int)Indices[i + 1]];
+                var v2 = (Vector3)Vertices[(int)Indices[i + 2]];
+
+                v2 -= v0;
+                v1 -= v0;
+
+                var vn = Vector3.Cross(v2, v1);
+
+                for( int j=0; j<3; ++j)
+                {
+                    int idx = (int) Indices[i + j];
+                    if (null == Normals[idx])
+                    {
+                        Normals[idx] = vn;
+                        normalCount[idx] = 1;
+                    }
+                    else
+                    {
+                        Normals[idx] = ((Vector3)Normals[idx]) + vn;
+                        normalCount[idx]++;
+                    }
+                }
+            }
+            for( int n=0; n<nVertices; ++n)
+            {
+                if( null==Normals[n])
+                {
+                    // Normal is not referenced, ignore it.
+                } else
+                {
+                    int nNormals = normalCount[n];
+                    if( nNormals>0 )
+                    {
+                        var vn = (Vector3) Normals[n];
+                        // TXWTODO: Do we really need this we normalize anyway.
+                        Normals[n] = vn / nNormals;
+                        vn = Vector3.Normalize(vn);
+                    }
+                }
+            }
+        }
 
         public Mesh( IList vertices, IList indices, IList uvs )
         {
             Vertices = vertices;
             Indices = indices;
             UVs = uvs;
+            Normals = null;
         }
 
         public static Mesh CreateArrayListInstance()
---
+++ Karawan/platform/cs1/splash/MeshGenerator.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Numerics;
+
+namespace Karawan.platform.cs1.splash
+{
+    public unsafe class MeshGenerator
+    {
+        public unsafe static void AllocateRaylibMesh( Raylib_CsLo.Mesh rlm, int nVertices, int nIndices )
+        {
+            rlm.vertexCount = nVertices;
+            rlm.triangleCount = nIndices / 3;
+            rlm.vertices = (float*)Raylib_CsLo.Raylib.MemAlloc((uint)(rlm.vertexCount * 3 * sizeof(float)));
+            rlm.texcoords = (float*)Raylib_CsLo.Raylib.MemAlloc((uint)(rlm.vertexCount * 2 * sizeof(float)));
+            rlm.normals = (float*)Raylib_CsLo.Raylib.MemAlloc((uint)(rlm.vertexCount * 3 * sizeof(float)));
+            rlm.indices = (ushort*)Raylib_CsLo.Raylib.MemAlloc((uint)(rlm.triangleCount * 3 * sizeof(ushort)));
+        }
+
+
+#if false
+
+void Smooth(Mesh* mesh, bool enabled = true)
+        {
+            Vector3[Vector3] linkedNormals;
+            size_t idx = 0;
+
+            foreach (tri; mesh.vertices[0..mesh.vertexCount * 3].chunks(9))
+    {
+                Vector3 v1 = Vector3(tri[0], tri[1], tri[2]);
+                Vector3 v2 = Vector3(tri[3], tri[4], tri[5]);
+                Vector3 v3 = Vector3(tri[6], tri[7], tri[8]);
+
+                Vector3 normalV1 = Vector3CrossProduct(Vector3Diff(v2, v1), Vector3Diff(v3, v1));
+                Vector3 normalV2 = Vector3CrossProduct(Vector3Diff(v3, v2), Vector3Diff(v1, v2));
+                Vector3 normalV3 = Vector3CrossProduct(Vector3Diff(v1, v3), Vector3Diff(v2, v3));
+
+                Vector3 sum = Vector3Add(Vector3Add(normalV1, normalV2), normalV3);
+
+                if (enabled)
+                {
+                    if (v1!in linkedNormals) linkedNormals[v1] = Vector3Zero;
+                    if (v2!in linkedNormals) linkedNormals[v2] = Vector3Zero;
+                    if (v3!in linkedNormals) linkedNormals[v3] = Vector3Zero;
+
+                    linkedNormals[v1] = Vector3Add(linkedNormals[v1], sum);
+                    linkedNormals[v2] = Vector3Add(linkedNormals[v2], sum);
+                    linkedNormals[v3] = Vector3Add(linkedNormals[v3], sum);
+                }
+                else
+                {
+                    Vector3 nor = Vector3Normalize(sum);
+
+                    foreach (k; 0..3)
+            {
+                        mesh.normals[idx + k * 3 + 0] = nor.x;
+                        mesh.normals[idx + k * 3 + 1] = nor.y;
+                        mesh.normals[idx + k * 3 + 2] = nor.z;
+                    }
+
+                }
+
+                idx += 9;
+            }
+
+            if (enabled)
+            {
+                idx = 0;
+                foreach (v; mesh.vertices[0..mesh.vertexCount * 3].chunks(3))
+        {
+                    Vector3 cur = Vector3(v[0], v[1], v[2]);
+                    Vector3 nor = Vector3Normalize(linkedNormals[cur]);
+
+                    mesh.normals[idx + 0] = nor.x;
+                    mesh.normals[idx + 1] = nor.y;
+                    mesh.normals[idx + 2] = nor.z;
+
+                    idx += 3;
+                }
+            }
+
+            rlUpdateMesh(*mesh, 2, mesh.vertexCount);
+        }
+#endif
+
+        public static Raylib_CsLo.Mesh CreateRaylibMesh( engine.joyce.components.Mesh mesh )
+        {
+            if( null==mesh.Normals )
+            {
+                mesh.GenerateNormals();
+            }
+            Raylib_CsLo.Mesh rlm = new();
+
+            var nVertices = mesh.Vertices.Count;
+            var nIndices = mesh.Indices.Count;
+            {
+                var nUVs = mesh.UVs.Count;
+                if (nUVs != nVertices)
+                {
+                    // TXWTODO: Throw an exception
+                    System.Console.WriteLine("Problem");
+                }
+            }
+
+            AllocateRaylibMesh(rlm, nVertices, nIndices);
+            for(int v=0; v<nVertices; v++)
+            {
+                Vector3 vertex = (Vector3) mesh.Vertices[v];
+                Vector2 uv = (Vector2)mesh.UVs[v];
+                rlm.vertices[v * 3 + 0] = vertex.X;
+                rlm.vertices[v * 3 + 1] = vertex.Y;
+                rlm.vertices[v * 3 + 2] = vertex.Z;
+                rlm.texcoords[v * 2 + 0] = uv.X;
+                rlm.texcoords[v * 2 + 1] = uv.Y;
+            }
+            return rlm;
+        }
+    }
+}
