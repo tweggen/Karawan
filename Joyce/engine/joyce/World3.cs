8a41e3e32505aca817ac9c8a5c345b171c867183
Timo Weggen <timo.weggen@gmail.com>
2023-01-27 08:09:58 +0100

    - push changes.

---
+++ Joyce/engine/joyce/World3.cs
@@ -0,0 +1,25 @@
+﻿
+namespace engine.joyce
+{
+    public class World3
+    {
+        private engine.Engine _engine;
+        private DefaultEcs.Entity _rootEntity;
+
+        public void Remove()
+        {
+        }
+
+        public void Add()
+        {
+        }
+
+        public World3(engine.Engine engine)
+        {
+            _engine = engine;
+            _rootEntity = _engine.GetEcsWorld().CreateEntity();
+            _rootEntity.Disable();
+            _rootEntity.Set<transform.components.Transform3ToParent>(new transform.components.Transform3ToParent());
+        }
+    }
+}
