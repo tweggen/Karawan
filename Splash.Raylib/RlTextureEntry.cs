19c6b45944cec126f371a3de57304b798f9beb01
Timo Weggen <63ce351416dfc2b1fbc9c97e@bots.bitbucket.org>
2023-03-21 16:07:22 +0100

    - snapshot.

--- Splash.Raylib/RlTextureEntry.cs
+++ Splash.Raylib/RlTextureEntry.cs
@@ -1,8 +1,15 @@
 ﻿
+using Raylib_CsLo;
+
 namespace Splash.Raylib
 {
-    public class RlTextureEntry
+    public class RlTextureEntry : Splash.ATextureEntry
     {
         public Raylib_CsLo.Texture RlTexture;
+
+        public override bool IsUploaded()
+        {
+            return RlTexture.width != 0;
+        }
     }
 }
---
+++ Splash/ATextureEntry.cs
@@ -0,0 +1,6 @@
+namespace Splash;
+
+public abstract class ATextureEntry
+{
+    public abstract bool IsUploaded();
+}
