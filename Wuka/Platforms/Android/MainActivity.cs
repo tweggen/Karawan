2ff163751082ff98352de7ee5270eb6406b482a6
Timo Weggen <63ce351416dfc2b1fbc9c97e@bots.bitbucket.org>
2023-04-03 17:39:03 +0200

    - KAR-20 #comment Android stuf- KAR-20 #comment Android stufff

--- Wuka/Platforms/Android/MainActivity.cs
+++ Wuka/Platforms/Android/MainActivity.cs
@@ -2,13 +2,13 @@
 using Android.App.Roles;
 using Android.Content.PM;
 using Android.OS;
-using Silk.NET.Windowing;
+// using Silk.NET.Windowing;
 using Silk.NET.Windowing.Sdl.Android;
 
 namespace Wuka
 {
     [Activity(MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : SilkActivity
+    public class MainActivity : Silk.NET.Windowing.Sdl.Android.SilkActivity
     {
         private Silk.NET.Windowing.IView _iView;
         protected override void OnRun()
@@ -18,8 +18,7 @@ namespace Wuka
             options.API = new GraphicsAPI(ContextAPI.OpenGLES, ContextProfile.Compatability, ContextFlags.Default, new APIVersion(3, 0));
             _iView = Silk.NET.Windowing.Window.GetView(options); // note also GetView, instead of Window.Create.
 
-
-            var e = Splash.Silk.Platform.EasyCreate(new string[] { }, iView);
+            var e = Splash.Silk.Platform.EasyCreate(new string[] { }, _iView);
             engine.GlobalSettings.Set("Engine.ResourcePath", "..\\..\\..\\..\\");
 
             e.AddSceneFactory("root", () => new nogame.RootScene());
