47d10e5d1635a4a429cc279e83e07608cdc9df26
Timo Weggen <timo.weggen@gmail.com>
2023-04-25 18:38:16 +0200

    - typed some letters.

--- JoyceCode/builtin/tools/Lindenmayer/AlphaInterpreter.cs
+++ JoyceCode/builtin/tools/Lindenmayer/AlphaInterpreter.cs
@@ -80,42 +80,42 @@ public class AlphaInterpreter
             // trace('Before part: rotation is ${state.rotation}.');
             // trace('Before part: position is ${state.position}.');
             // trace('Now part ${part.name}');
-            var p = part.Parameters;
+            var p = part.Parameters.Map;
             if (part.Name == "rotate(d,x,y,z)")
             {
                 var qrot = Quaternion.CreateFromAxisAngle(
                     new Vector3(p["x"], p["y"], p["z"],
-                    p["d"] / 180.*Math.PI
+                    p["d"] / 180f*(float)System.Math.PI
                     )
                 );
-                state.rotation.multiplyAsA(qrot);
+                state.Rotation = qrot * state.Rotation;
             }
-            else if (part.name == "fillrgb(r,g,b)")
+            else if (part.Name == "fillrgb(r,g,b)")
             {
-                state.color = new geom.Vector3D(p["r"], p["g"], p["b"]);
+                state.Color = new Vector3(p["r"], p["g"], p["b"]);
             }
-            else if (part.name == "cyl(r,l)")
+            else if (part.Name == "cyl(r,l)")
             {
 
-                var vs = state.position.clone();
+                var vs = state.Position;
 
                 /*
                  * vd shall be scaled with l
                  */
-                var vd = new geom.Vector3D(1., 0., 0.);
-                vd.applyQuaternion(state.rotation);
+                var vd = new Vector3(1f, 0f, 0f);
+                vd = Vector3.Transform(vd, state.Rotation);
+
                 /*
                  * vRadius shall be scaled with r
                  */
-                var vr = new geom.Vector3D(0., 1., 0.);
-                vr.applyQuaternion(state.rotation);
+                var vr = new Vector3(0f, 1f, 0f);
+                vr = Vector3.Transform(vr, state.Rotation);
 
-                var vt = vr.clone();
-                vt.cross(vd);
+                Vector3 vt = Vector3.Cross(vd, vr);
 
-                vd.scale(p["l"]);
-                vr.scale(p["r"]);
-                vt.scale(p["r"]);
+                vd *= p["l"];
+                vr *= p["r"];
+                vt *= p["r"];
                 // trace( 'LAlphaInterpreter.run(): From ${vs} direction ${vd} radius ${vr}.' );
 
                 /*
