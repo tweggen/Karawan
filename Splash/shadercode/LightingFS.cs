5c42aaac97e4b14d7a5ea2f3a2a6ca021323fb9d
Timo Weggen <timo.weggen@gmail.com>
2023-05-19 18:41:40 +0200

    - fixed the neon signs

--- Splash/shadercode/LightingFS.cs
+++ Splash/shadercode/LightingFS.cs
@@ -100,9 +100,6 @@ void main()
     }
 
     
-    // finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
-    // finalColor += texelColor*(ambient)*colDiffuse;
-    // finalColor = texelColor+colDiffuse;
     vec4 colDiffuseTotal = texelColor + colDiffuse;
     vec4 colEmissiveTotal = emissiveColor; 
     vec4 colAmbientTotal = ambient; 
@@ -111,12 +108,7 @@ void main()
         + colEmissiveTotal
         //+ vec4(0.53,0.15,0.18,0.0)
         + colAmbientTotal
-        ;
-    if (finalColor.a <= 1.0)
-    {
-        finalColor = vec4( colEmissiveTotal.r, colEmissiveTotal.g, colEmissiveTotal.b, 0.0);
-    }
-    
+        ;    
 
     // Gamma correction
     // finalColor = pow(finalColor, vec4(1.0/2.2));
