f8ead79e912879025f5bea9d7fb97b5a0072ae69
Timo Weggen <timo.weggen@gmail.com>
2023-07-01 15:15:52 +0200

    - adjusted volume levels

--- nogameCode/nogame/characters/car3/GenerateCharacterOperator.cs
+++ nogameCode/nogame/characters/car3/GenerateCharacterOperator.cs
@@ -27,7 +27,7 @@ class GenerateCharacterOperator : engine.world.IFragmentOperator
                 for (int j = 0; j < 3; ++j)
                 {
                     _jCar3Sound[j] = new engine.audio.Sound(
-                        "car3noisemono.ogg", true, 0.12f, 0.7f + j*0.3f);
+                        "car3noisemono.ogg", true, 0.25f, 0.7f + j*0.3f);
                 }
             }
 
