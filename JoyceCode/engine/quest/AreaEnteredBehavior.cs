bea4290e68a32331a20be97ab4bf6130e2881b76
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-01-12 17:00:29 +0100

    - small refactoring.

--- JoyceCode/engine/quest/AreaEnteredBehavior.cs
+++ JoyceCode/engine/quest/AreaEnteredBehavior.cs
@@ -2,12 +2,29 @@
 using DefaultEcs;
 using engine.behave;
 using engine.physics;
+using BepuPhysics;
+using System.Numerics;
 using static engine.Logger;
 
 namespace engine.quest;
 
+/**
+ * Defines a quest to go to a particular location.
+ *
+ * Creating this quest involves creating an entity representing the quest.
+ *
+ * The entity may place various ancillary entities that would disappear
+ * after the quest, such as a collision shape to identify the target
+ * location.
+ */
 public class AreaEnteredBehavior : IBehavior
 {
+    private engine.Engine _engine;
+    private BodyReference _prefCylinder;
+    private engine.physics.Object _poCylinder;
+    private DefaultEcs.Entity _eGoal;
+
+
     public void OnCollision(ContactEvent cev)
     {
         
@@ -27,5 +44,26 @@ public class AreaEnteredBehavior : IBehavior
 
     public void OnAttach(in Engine engine0, in Entity entity)
     {
+        BodyHandle phandleCylinder;
+        
+        lock (_engine.Simulation)
+        {
+            phandleCylinder = _engine.Simulation.Bodies.Add(
+                BodyDescription.CreateKinematic(
+                    new Vector3(0f, 0f, 0f), // infinite mass, this is a kinematic object.
+                    new BepuPhysics.Collidables.CollidableDescription(
+                        engine.physics.ShapeFactory.GetCylinderShape(3f, _engine),
+                        0.1f),
+                    new BodyActivityDescription(0.01f)
+                )
+            );
+            
+            _prefCylinder = _engine.Simulation.Bodies.GetBodyReference(phandleCylinder);
+            /*
+             * Position will be set by setup kinetics system.
+             */
+        }
+        _poCylinder = new engine.physics.Object(_eGoal, phandleCylinder);
+
     }
 }
--- JoyceCode/engine/quest/ToLocation.cs
+++ JoyceCode/engine/quest/ToLocation.cs
@@ -1,26 +1,13 @@
-using BepuPhysics;
-using System.Numerics;
+
 
 namespace engine.quest;
 
 
 /**
- * Defines a quest to go to a particular location.
- *
- * Creating this quest involves creating an entity representing the quest.
- *
- * The entity may place various ancillary entities that would disappear
- * after the quest, such as a collision shape to identify the target
- * location.
+ * Might define a quest tracker. Does not. 
  */
 public class ToLocation : AModule
 {
-    public Vector3 TargetLocation { get; set; }
-
-    private BodyReference _prefCylinder;
-    private engine.physics.Object _poCylinder;
-    private DefaultEcs.Entity _entity;
-
     public override void ModuleDeactivate()
     {
         _engine.RemoveModule(this);
@@ -33,20 +20,5 @@ public class ToLocation : AModule
         base.ModuleActivate(engine0);
         engine0.AddModule(this);
 
-        lock (_engine.Simulation)
-        {
-            BodyHandle phandleCylinder = _engine.Simulation.Bodies.Add(
-                BodyDescription.CreateKinematic(
-                    new Vector3(0f, 0f, 0f), // infinite mass, this is a kinematic object.
-                    new BepuPhysics.Collidables.CollidableDescription(
-                        engine.physics.ShapeFactory.GetCylinderShape(3f, _engine),
-                        0.1f),
-                    new BodyActivityDescription(0.01f)
-                )
-            );
-            _poCylinder = new engine.physics.Object(
-            _prefCylinder = _engine.Simulation.Bodies.GetBodyReference(phandleCylinder);
-            _prefCylinder.Pose.Position = TargetLocation;
-        }
     }
 }
