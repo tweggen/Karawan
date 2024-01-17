e8be587a7ea8f5f1268f73b0b43cd191b1f21b8b
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-01-17 17:55:16 +0100

    - added new activity.

---
+++ Wuka/Platforms/Android/GameActivity.cs
@@ -0,0 +1,144 @@
+﻿using Android.App;
+using Android.App.Roles;
+using Android.Content.PM;
+using Android.OS;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Sdl;
+using Silk.NET.Input.Sdl;
+
+using Android.Content.Res;
+using engine;
+using Silk.NET.GLFW;
+using Android.Media;
+using Wuka.Platforms.Android;
+using Java.Util;
+using System.Numerics;
+using Android;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+using Xamarin.Essentials;
+using nogame;
+using GameState = Android.App.GameState;
+
+namespace Wuka
+{
+    [Activity(
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+        ScreenOrientation = ScreenOrientation.Landscape,
+        Theme = "@style/Maui.SplashTheme" //"@android:style/Theme.Black.NoTitleBar.Fullscreen"
+    )]
+    public class GameActivity : Silk.NET.Windowing.Sdl.Android.SilkActivity, ActivityCompat.IOnRequestPermissionsResultCallback
+    {
+        private object _lo = new();
+        private bool _triggeredGame = false;
+
+        internal static AssetManager AssetManager;
+
+        private Silk.NET.Windowing.IView _iView;
+        private engine.Engine _engine;
+
+
+
+        protected override void OnStop()
+        {
+            try
+            {
+                /*
+                 * Try to save a backup copy
+                 */
+                I.Get<engine.DBStorage>()?.SaveGameState(I.Get<GameState>());
+            }
+            catch (Exception e)
+            {
+                // It is perfectly ok we didn't have the DB or the gamestate yet.
+            }
+
+            // _engine.Suspend();
+            _engine?.Exit();
+
+            base.OnStop();
+        }
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+
+        protected override void OnRestart()
+        {
+            //_engine.Resume();
+            base.OnRestart();
+            _triggerGame();
+        }
+
+
+        void _triggerGame()
+        {
+            lock (_lo)
+            {
+                if (_triggeredGame)
+                {
+                    return;
+                }
+                _triggeredGame = true;
+            }
+
+            /*
+             * Setup singletons and statics
+             */
+            var assetManagerImplementation = new Wuka.AssetImplementation(Assets);
+            engine.Assets.SetAssetImplementation(assetManagerImplementation);
+
+            var options = ViewOptions.Default;
+            options.API = new GraphicsAPI(ContextAPI.OpenGLES, ContextProfile.Compatability, ContextFlags.Default, new APIVersion(3, 0));
+            options.FramesPerSecond = 60;
+            options.VSync = false;
+            options.ShouldSwapAutomatically = false;
+            _iView = Silk.NET.Windowing.Window.GetView(options); // note also GetView, instead of Window.Create.
+
+            engine.GlobalSettings.Set("nogame.CreateOSD", "true");
+            engine.GlobalSettings.Set("platform.threeD.API", "OpenGLES");
+            engine.GlobalSettings.Set("platform.threeD.API.version", "300");
+            engine.GlobalSettings.Set("engine.NailLogicalFPS", "true");
+            engine.GlobalSettings.Set("Engine.ResourcePath", "./");
+            engine.GlobalSettings.Set("splash.touchControls", "true");
+            engine.GlobalSettings.Set("Android", "true");
+            engine.GlobalSettings.Set("platform.initialZoomState", "-16");
+            engine.GlobalSettings.Set("nogame.CreateUI", "false");
+            engine.GlobalSettings.Set("nogame.LogosScene.PlayTitleMusic", "true");
+            engine.GlobalSettings.Set("Engine.RWPath", System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData));
+
+            _engine = Splash.Silk.Platform.EasyCreate(new string[] { }, _iView);
+            _iView.Initialize();
+
+            I.Register<Boom.ISoundAPI>(() =>
+            {
+                var api = new Boom.OpenAL.API(_engine);
+                return api;
+            });
+
+            nogame.Main.Start(_engine);
+
+            _engine.Execute();
+
+            I.Get<Boom.ISoundAPI>().Dispose();
+
+        }
+
+        protected override void OnRun()
+        {
+            /*
+             * setup framework dependencies.
+             */
+            SdlWindowing.RegisterPlatform();
+            SdlInput.RegisterPlatform();
+
+            _triggerGame();
+        }
+    }
+}
