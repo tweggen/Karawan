f2d48bb0e14150876672c182a73d9a7f7992fb26
Timo Weggen <timo.weggen@gmail.com>
2024-02-03 10:55:36 +0100

    - put the debugging quest marker to a reasonable height.

--- nogameCode/nogame/quests/VisitAgentTwelve/Quest.cs
+++ nogameCode/nogame/quests/VisitAgentTwelve/Quest.cs
@@ -49,7 +49,7 @@ public class Quest : AModule, IQuest
          */
         var newQuestTarget = new engine.quest.ToLocation()
         {
-            RelativePosition = new Vector3(-407f, 100f, 396f),
+            RelativePosition = new Vector3(-440f, 80f, 389f),
             SensitivePhysicsName = nogame.modules.playerhover.Module.PhysicsName,
             MapCameraMask = nogame.modules.map.Module.MapCameraMask
         };
