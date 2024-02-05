4f185a06c3accb6dcc4ff008201daeed077a12cd
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-02-05 13:55:58 +0100

    - fixed rom.loader for android and windows.

--- JoyceCode/engine/rom/Loader.cs
+++ JoyceCode/engine/rom/Loader.cs
@@ -73,116 +73,131 @@ public class Loader
     {
         var visited = new SortedDictionary<string, Assembly>();
         var queue = new Queue<Assembly>();
-        
-        /*
-         * Load the given assembly if not yet done.
-         */
+
         try
         {
-            if (!_mapAlreadyLoaded.ContainsKey(dllPath))
-            {
-                /*
-                 * Add both the user name and the real name.
-                 */
-                var asmGiven = TryLoadDll(dllPath);
-                if (null != asmGiven)
-                {
-                    _mapAlreadyLoaded.Add(asmGiven.GetName().Name, asmGiven);
-                    _mapAlreadyLoaded.Add(dllPath, asmGiven);
-                    queue.Enqueue(asmGiven);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            Trace($"Unable to load the given assembly {dllPath} into the desired context: {e}");
-        }
-
-        /*
-         * In addition, queue the entry assmebly...
-         */
-        Assembly asmEntry = Assembly.GetEntryAssembly();
-        if (!_mapAlreadyLoaded.ContainsKey(asmEntry.GetName().Name))
-        {
-            _mapAlreadyLoaded.Add(asmEntry.GetName().Name, asmEntry);
-            queue.Enqueue(asmEntry);
-        }
-
-        /*
-         * ... and all other known assemblies.
-         */
-        foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            if (!_mapAlreadyLoaded.ContainsKey(asm.GetName().Name))
-            {
-                _mapAlreadyLoaded.Add(asm.GetName().Name, asm);
-                queue.Enqueue(asm);
-            }
-        }
-        
-        /*
-         * Finally, dump all the assemblies we use to find the target.
-         */
-        foreach (var asm in queue)
-        {
-            Trace($"Starting resolve with {asm.GetName().Name}");
-        }
-
-        /*
-         * Iterate through all dlls we should load and check.
-         */
-        while (queue.Any())
-        {
-            var asm = queue.Dequeue();
-
             /*
-             * If we already traced its dependencies, ignore it.
+             * Load the given assembly if not yet done.
              */
-            if (visited.ContainsKey(asm.GetName().Name))
+            try
             {
-                continue;
-            }
-
-            Trace($"Adding dll {asm.GetName().Name}");
-            visited.Add(asm.GetName().Name, asm);
-            var references = asm.GetReferencedAssemblies();
-            foreach (var anRef in references)
-            {
-                if (_setLoadFailed.Contains(anRef.Name))
+                if (!_mapAlreadyLoaded.ContainsKey(dllPath))
                 {
                     /*
-                     * Skip re-load of already failed load.
+                     * Add both the user name and the real name.
                      */
+                    var asmGiven = TryLoadDll(dllPath);
+                    if (null != asmGiven)
+                    {
+                        _mapAlreadyLoaded.Add(asmGiven.GetName().Name, asmGiven);
+                        _mapAlreadyLoaded.Add(dllPath, asmGiven);
+                        queue.Enqueue(asmGiven);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace($"Unable to load the given assembly {dllPath} into the desired context: {e}");
+            }
+
+            /*
+             * In addition, queue the entry assmebly...
+             */
+            Assembly asmEntry = Assembly.GetEntryAssembly();
+            if (null != asmEntry)
+            {
+                if (!_mapAlreadyLoaded.ContainsKey(asmEntry.GetName().Name))
+                {
+                    _mapAlreadyLoaded.Add(asmEntry.GetName().Name, asmEntry);
+                    queue.Enqueue(asmEntry);
+                }
+            }
+
+            /*
+             * ... and all other known assemblies.
+             */
+            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (null != asm)
+                {
+                    if (!_mapAlreadyLoaded.ContainsKey(asm.GetName().Name))
+                    {
+                        _mapAlreadyLoaded.Add(asm.GetName().Name, asm);
+                        queue.Enqueue(asm);
+                    }
+                }
+            }
+
+            /*
+             * Finally, dump all the assemblies we use to find the target.
+             */
+            foreach (var asm in queue)
+            {
+                Trace($"Starting resolve with {asm.GetName().Name}");
+            }
+
+            /*
+             * Iterate through all dlls we should load and check.
+             */
+            while (queue.Any())
+            {
+                var asm = queue.Dequeue();
+
+                /*
+                 * If we already traced its dependencies, ignore it.
+                 */
+                if (visited.ContainsKey(asm.GetName().Name))
+                {
                     continue;
                 }
 
-                /*
-                 * Try to load the assembly if not yet loaded.
-                 */
-                if (!_mapAlreadyLoaded.ContainsKey(anRef.Name))
+                Trace($"Adding dll {asm.GetName().Name}");
+                visited.Add(asm.GetName().Name, asm);
+                var references = asm.GetReferencedAssemblies();
+                foreach (var anRef in references)
                 {
-                    try
+                    if (_setLoadFailed.Contains(anRef.Name))
                     {
-                        var asmRef = Assembly.Load(anRef);
-                        _mapAlreadyLoaded.Add(anRef.Name, asmRef);
+                        /*
+                         * Skip re-load of already failed load.
+                         */
+                        continue;
                     }
-                    catch (Exception ex)
-                    {
-                        Trace($"Failed to load {anRef.Name}: {ex}");
-                        _setLoadFailed.Add(anRef.Name);
-                    }
-                }
 
-                /*
-                 * If we could load it, queue it's dependencies to be analysed.
-                 */
-                if (_mapAlreadyLoaded.ContainsKey(anRef.Name))
-                {
-                    queue.Enqueue(Assembly.Load(anRef));
+                    /*
+                     * Try to load the assembly if not yet loaded.
+                     */
+                    if (!_mapAlreadyLoaded.ContainsKey(anRef.Name))
+                    {
+                        try
+                        {
+                            var asmRef = Assembly.Load(anRef);
+                            _mapAlreadyLoaded.Add(anRef.Name, asmRef);
+                        }
+                        catch (Exception ex)
+                        {
+                            Trace($"Failed to load {anRef.Name}: {ex}");
+                            _setLoadFailed.Add(anRef.Name);
+                        }
+                    }
+
+                    /*
+                     * If we could load it, queue it's dependencies to be analysed.
+                     */
+                    if (_mapAlreadyLoaded.ContainsKey(anRef.Name))
+                    {
+                        queue.Enqueue(Assembly.Load(anRef));
+                    }
                 }
             }
+
         }
-        
+        catch (Exception e)
+        {
+            Trace($"Exception {e}");
+        }
+
+        // .. Fallback
         /*
          * Finally, return everything that has been loaded.
          */
