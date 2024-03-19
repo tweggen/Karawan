cc72baa21c0c25698b155ffb6e33e831d7b81e6f
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-03-19 10:44:04 +0100

    - made decisions.

--- JoyceCode/engine/behave/systems/SpawnSystem.cs
+++ JoyceCode/engine/behave/systems/SpawnSystem.cs
@@ -12,6 +12,14 @@ using static engine.Logger;
 namespace engine.behave.systems;
 
 
+/**
+ * This is the system to apply mass manipulation to behaviors.
+ *
+ * - it counts every behavior that has a spawn operator attached.
+ * - it triggers deletion for characters it finds
+ *   - using a comparator function provided by the operator,
+ *   - using a deletion function provided by the operator.
+ */
 [DefaultEcs.System.With(typeof(engine.behave.components.Behavior))]
 [DefaultEcs.System.With(typeof(engine.joyce.components.Transform3ToWorld))]
 public class SpawnSystem : DefaultEcs.System.AEntitySetSystem<BehaviorStats>
