c08bb38fa832a562b6552e7a9d47c1b84871bfc7
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-04-24 12:06:02 +0200

    - fixed a major performance problem introduced earlier

--- Splash/AMeshEntry.cs
+++ Splash/AMeshEntry.cs
@@ -3,11 +3,31 @@ using System.Numerics;
 
 namespace Splash;
 
-public class AMeshParams
+public class AMeshParams : IEquatable<AMeshParams>
 {
     public engine.joyce.Mesh JMesh;
     public Vector2 UVOffset;
     public Vector2 UVScale;
+
+    public bool Equals(AMeshParams other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Equals(JMesh, other.JMesh) && UVOffset.Equals(other.UVOffset) && UVScale.Equals(other.UVScale);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((AMeshParams)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(JMesh, UVOffset, UVScale);
+    }
 }
 
 
