8da34247c87650570ffa434f6404dcf502e4e3e0
timow <timo.weggen@gmail.com>
2024-05-23 17:31:15 +0200

    - added generic component manager implementation.

---
+++ JoyceCode/engine/AComponentManager.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using static engine.Logger;
+
+namespace engine;
+
+public abstract class AComponentManager<T> where T : class
+{
+    private engine.Engine _engine;
+
+    private IDisposable? _subscriptions;
+
+    
+    public abstract void _onComponentRemoved(
+        in DefaultEcs.Entity entity,
+        in T cOldComponent);
+
+    
+    public abstract void _onComponentChanged(
+        in DefaultEcs.Entity entity,
+        in T cOldComponent,
+        in T cNewComponent);
+
+
+    public abstract void _onComponentAdded(
+        in DefaultEcs.Entity entity,
+        in T cNewComponent);
+
+
+    public void Stop()
+    {
+        if (_subscriptions != null)
+        {
+            _subscriptions.Dispose();
+            _subscriptions = null;
+        }
+    }
+    
+    
+    public void Manage(in engine.Engine engine)
+    {
+        _engine = engine;
+        
+        IEnumerable<IDisposable> GetSubscriptions(DefaultEcs.World w)
+        {
+            yield return w.SubscribeEntityComponentAdded<T>(_onComponentAdded);
+            yield return w.SubscribeEntityComponentChanged<T>(_onComponentChanged);
+            yield return w.SubscribeEntityComponentRemoved<T>(_onComponentRemoved);
+        }
+        DefaultEcs.World world = _engine.GetEcsWorld();
+        if (null == world)
+        {
+            ErrorThrow("world must not be null.", (m) => new ArgumentException(m));
+        }
+
+        {
+            var entities = world.GetEntities().With<T>().AsEnumerable();
+            foreach (DefaultEcs.Entity entity in entities)
+            {
+                _onComponentAdded(entity, entity.Get<T>());
+            }
+        }
+
+        _subscriptions = GetSubscriptions(world).Merge();
+    }
+
+
+    public void Dispose()
+    {
+        Stop();
+        _engine = null;
+    }
+}
