ce352e632ec14b43b3eba2ad7208d523712db685
Timo Weggen <timo.weggen@gmail.com>
2024-10-19 15:12:08 +0200

    - some refactoring.

--- Splash.Silk/BufferObject.cs
+++ Splash.Silk/BufferObject.cs
@@ -3,51 +3,49 @@ using System;
 using System.Diagnostics;
 using static engine.Logger;
 
-namespace Splash.Silk
+namespace Splash.Silk;
+
+/**
+ * Shamelessly copied from the tutorial
+ */
+public class BufferObject<TDataType> : IDisposable
+    where TDataType : unmanaged
 {
-    /**
-     * Shamelessly copied from the tutorial
-     */
-    public class BufferObject<TDataType> : IDisposable
-        where TDataType : unmanaged
+    //Our handle, buffertype and the GL instance this class will use, these are private because they have no reason to be public.
+    //Most of the time you would want to abstract items to make things like this invisible.
+    private uint _handle;
+    private BufferTargetARB _bufferType;
+    private GL _gl;
+
+    public unsafe BufferObject(GL gl, Span<TDataType> data, BufferTargetARB bufferType)
     {
-        //Our handle, buffertype and the GL instance this class will use, these are private because they have no reason to be public.
-        //Most of the time you would want to abstract items to make things like this invisible.
-        private uint _handle;
-        private BufferTargetARB _bufferType;
-        private GL _gl;
+        //Setting the gl instance and storing our buffer type.
+        _gl = gl;
+        _bufferType = bufferType;
 
-        public unsafe BufferObject(GL gl, Span<TDataType> data, BufferTargetARB bufferType)
+        //Getting the handle, and then uploading the data to said handle.
+        _handle = _gl.GenBuffer();
+        _gl.BindBuffer(_bufferType, _handle);
+        fixed (void* d = data)
         {
-            //Setting the gl instance and storing our buffer type.
-            _gl = gl;
-            _bufferType = bufferType;
-
-            //Getting the handle, and then uploading the data to said handle.
-            _handle = _gl.GenBuffer();
-            BindBuffer();
-            fixed (void* d = data)
-            {
-                _gl.BufferData(bufferType, (nuint) (data.Length * sizeof(TDataType)), d, BufferUsageARB.StaticDraw);
-                //_gl.Flush();
-            }
-
-            if (_gl.GetError() != GLEnum.NoError)
-            {
-                Trace("Error uploading mesh.");
-            }
+            _gl.BufferData(bufferType, (nuint)(data.Length * sizeof(TDataType)), d, BufferUsageARB.StaticDraw);
         }
 
-        public void BindBuffer()
+        if (_gl.GetError() != GLEnum.NoError)
         {
-            //Binding the buffer object, with the correct buffer type.
-            _gl.BindBuffer(_bufferType, _handle);
+            Trace("Error uploading mesh.");
         }
+    }
 
-        public void Dispose()
-        {
-            //Remember to delete our buffer.
-            _gl.DeleteBuffer(_handle);
-        }
+    public void BindBuffer()
+    {
+        //Binding the buffer object, with the correct buffer type.
+        _gl.BindBuffer(_bufferType, _handle);
+    }
+
+    public void Dispose()
+    {
+        //Remember to delete our buffer.
+        _gl.DeleteBuffer(_handle);
     }
 }
