f1ce548e4fbe9c0c0b275a2797240d6fb7f46398
timow <timo.weggen@gmail.com>
2024-11-09 11:14:29 +0100

    - added some stats to render frame

--- Splash/RenderFrame.cs
+++ Splash/RenderFrame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using engine.joyce.components;
 
@@ -6,6 +7,9 @@ namespace Splash
 {
     public class RenderFrame
     {
+        public uint FrameNumber;
+        public DateTime StartCollectTime;
+        public DateTime EndCollectTime;
         public LightCollector LightCollector = new();
         public FrameStats FrameStats = new();
         
