13dee83586f142d9a8d1761c748149c84d1e16f7
Timo Weggen <timo.weggen@gmail.com>
2024-12-20 18:57:30 +0100

    - changes in the size of the streets.

--- JoyceCode/engine/streets/Stroke.cs
+++ JoyceCode/engine/streets/Stroke.cs
@@ -295,7 +295,7 @@ namespace engine.streets
             var w = Weight;
             //w = w - 0.5;
             if (w < 0f ) w = 0f;
-            return 0.5f+1.2f * (9.5f + w * 12f);
+            return 6f+ w * 9f;
 #endif
         }
 
