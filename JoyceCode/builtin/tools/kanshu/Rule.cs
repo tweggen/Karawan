1f0196fa633b0cb8b0d62d5549c1bf08bc783f53
Timo Weggen <timo.weggen@gmail.com>
2025-01-25 19:36:41 +0100

    - added some ideas and todfos.,

--- JoyceCode/builtin/tools/kanshu/Rule.cs
+++ JoyceCode/builtin/tools/kanshu/Rule.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace builtin.tools.kanshu;
 
 public class Rule<TNodeLabel,TEdgeLabel>
 {
+    public float Probability;
+
+    // TXWTODO: Define call context as done for lsystems.
+    // TXWTODO: Unify the context for lua calls, the context for Lsystems and this
+    // TXWTODO: Decide when to stop all these genericsd.
+    // public Func<bool> Condition;
     public Pattern<TNodeLabel, TEdgeLabel> Pattern { get; init; }
     public Graph<TNodeLabel, TEdgeLabel> Replacement { get; init; }
 }
