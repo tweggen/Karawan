8758ab136af854e632c495197fc7914c573908fd
Timo Weggen <timo.weggen@rohde-schwarz.com>
2025-02-08 14:07:52 +0100

    - added rules and continued with rule.

--- JoyceCode/builtin/tools/Lindenmayer/Rule.cs
+++ JoyceCode/builtin/tools/Lindenmayer/Rule.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 
 namespace builtin.tools.Lindenmayer;
 
+
+/**
+ * A lindenmayer system rule.
+ * Rules are matched by name.
+ * The parameters of the original part are passed to the transforming
+ * function.
+ */
 public class Rule
 {
+    /**
+     * Lindenmayer rules are matched directly by name.
+     */
     public string Name;
+    
+    /**
+     * The probability of this rule to match.
+     */
     public float Probability;
+    
+    /**
+     * Only if this condition matches, the rule matches.
+     */
     public Func<Params, bool> Condition;
+    
+    /**
+     * Returns the parts that shall replace the part that
+     * matched.
+     */
     public Func<Params, IList<Part>> TransformParts;
-
+    
+    /**
+     * Built-in condition to always match. 
+     */
     public static bool Always(Params _) => true;
 
     public Rule(
--- JoyceCode/builtin/tools/kanshu/Rule.cs
+++ JoyceCode/builtin/tools/kanshu/Rule.cs
@@ -4,12 +4,36 @@ namespace builtin.tools.kanshu;
 
 public class Rule<TNodeLabel,TEdgeLabel>
 {
-    public float Probability;
+    /**
+     * Lindenmayer rules are matched directly by name.
+     */
+    public string Name { get; set; }
+    
+    /**
+     * The probability of this rule to match.
+     */
+    public float Probability { get; set; }
+    
+    /**
+     * Only if this condition matches, the rule matches.
+     */
+    public Func<Properties, bool> Condition { get; set; }
+
 
     // TXWTODO: Define call context as done for lsystems.
     // TXWTODO: Unify the context for lua calls, the context for Lsystems and this
     // TXWTODO: Decide when to stop all these genericsd.
     // public Func<bool> Condition;
-    public Pattern<TNodeLabel, TEdgeLabel> Pattern { get; init; }
-    public Graph<TNodeLabel, TEdgeLabel> Replacement { get; init; }
+    public Pattern<TNodeLabel, TEdgeLabel> Pattern { get; set; }
+    
+    /**
+     * A function replacing the input parts with a given replacement
+     * graph.
+     *
+     * @param properties
+     *     the bound properties of the matched pattern
+     * @returns
+     *     a graph to replace the original patter with.
+     */
+    public Func<Properties, Graph<TNodeLabel, TEdgeLabel>> Replacement { get; set; }
 }
