45215d52534c09cb7320a86bf77fceb191b7b6c0
Timo Weggen <timo.weggen@gmail.com>
2025-02-16 19:53:02 +0100

    - drafted an API for replacement of nodes.

--- JoyceCode/builtin/tools/kanshu/ConstantReplacement.cs
+++ JoyceCode/builtin/tools/kanshu/ConstantReplacement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace builtin.tools.kanshu;
 
@@ -7,8 +8,15 @@ public class ConstantReplacement
     /**
      * Provide a replacement made from a list of edges and nodes created by
      * transformation from existing ones plus a list of newly specified edges and nodes.
+     *
+     * @param replaceByNodes
+     *     Node i from the pattern shall be replaced by the given node.
      */
-    public static Func<Graph, MatchResult, Graph?> Create()
+    public static Func<Graph, MatchResult, Graph?> Create(
+        SortedDictionary<int, NodeDescriptor> replaceByNodes,
+        SortedDictionary<int, EdgeDescriptor> replaceByEdges,
+        List<NodeDescriptor> newNodes,
+        List<EdgeDescriptor> newEdges)
     {
         return (graph, matchResult) =>
         {
--- JoyceCode/builtin/tools/kanshu/MatchResult.cs
+++ JoyceCode/builtin/tools/kanshu/MatchResult.cs
@@ -4,6 +4,9 @@ namespace builtin.tools.kanshu;
 
 public class MatchResult
 {
+    /**
+     * Node template n from the pattern matches the given node in real life.
+     */
     public Dictionary<int, Graph.Node> Nodes { get; set; }
 
     public Rule Rule { get; set; }
