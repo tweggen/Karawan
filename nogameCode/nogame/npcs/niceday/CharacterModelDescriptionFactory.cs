b211d2f678d052c9d5d7ecc31b90582816875495
tweggen <timo.weggen@gmail.com>
2025-12-17 15:16:21 +0100

    - snapshot.

---
+++ nogameCode/nogame/npcs/niceday/CharacterModelDescriptionFactory.cs
@@ -0,0 +1,54 @@
+using System.Numerics;
+using builtin.tools;
+using engine.geom;
+
+namespace nogame.npcs.niceday;
+
+using engine.joyce;
+
+public static class CharacterModelDescriptionFactory
+{
+    private static readonly string [] _arrModels = 
+    {
+        "man_coat_winter_Rig.fbx",
+    };
+
+    public static nogame.characters.CharacterModelDescription CreateCitizen(RandomSource rnd)
+    {
+        string strModel;
+        int idxModel = (int)(rnd.GetFloat() * _arrModels.Length);
+        strModel = _arrModels[idxModel];        
+        bool isMale = strModel.StartsWith("man");
+        float propMaxDistance = (float)engine.Props.Get("nogame.npcs.maxDistance", 200f);
+        
+        return new()
+        {
+            IdleAnimName = "Idle_HardDay",
+            AnimationUrls = $"Idle_HardDay.fbx;Run_InPlace.fbx;{(isMale?"Walk_Male.fbx":"Walk_InPlace_Female.fbx")};Death_FallForwards.fbx",
+            ModelBaseBone = "Root_M",
+            ModelUrl = strModel,
+            Scale = "1",
+            InstantiateModelParams = new()
+            {               
+                GeomFlags = 0
+                            | InstantiateModelParams.BUILD_PHYSICS
+                            /*
+                             * We better have the cititen non-tangible to not stop the
+                             * car or anybody running through them. They may, however, react.
+                             */ 
+                            // | InstantiateModelParams.PHYSICS_TANGIBLE
+                            | InstantiateModelParams.PHYSICS_DETECTABLE
+                            | InstantiateModelParams.PHYSICS_CALLBACKS
+                ,
+                MaxVisibilityDistance = propMaxDistance,
+                MaxBehaviorDistance = propMaxDistance,
+                MaxAudioDistance = propMaxDistance,
+                MaxPhysicsDistance = 4f,
+            
+                PhysicsAABB = new AABB(new Vector3(-0.30f, 0f, -0.15f), new Vector3(0.3f, 1.85f, 0.15f)),
+                CollisionLayers = 0x0002,
+            }
+        };
+    }
+
+}
--- nogameCode/nogame/npcs/niceday/FragmentOperator.cs
+++ nogameCode/nogame/npcs/niceday/FragmentOperator.cs
@@ -7,6 +7,8 @@ using engine.geom;
 using engine.joyce;
 using engine.streets;
 using engine.world;
+using nogame.characters;
+using nogame.characters.citizen;
 using static engine.Logger;
 
 namespace nogame.npcs.niceday;
@@ -17,6 +19,14 @@ namespace nogame.npcs.niceday;
  */
 public class FragmentOperator : IFragmentOperator
 {
+    private class Context
+    {
+        public builtin.tools.RandomSource Rnd;
+        public engine.world.Fragment Fragment;
+    }
+    
+    public static readonly string EntityName = "nogame.npcs.niceguy";
+
     static private object _lo = new();
     private engine.world.ClusterDesc _clusterDesc;
     private AABB _clusterAABB;
@@ -35,6 +45,22 @@ public class FragmentOperator : IFragmentOperator
     }
 
 
+    private void _placeNPC(Vector3 v3Pos)
+    {
+        /*
+         * Right now, this character does not have any behavior.
+         */
+
+        var cmd = CharacterModelDescriptionFactory.CreateNPC(_rnd);
+        EntityCreator creator = new()
+        {
+            // no BehaviorFactory
+            CharacterModelDescription = cmd,
+            PhysicsName = EntityName
+        };
+    }
+    
+
     public Func<Task> FragmentOperatorApply(Fragment worldFragment, FragmentVisibility visib)
     {
         /*
@@ -50,16 +76,27 @@ public class FragmentOperator : IFragmentOperator
                 return;
             }
 
+            var ctx = new Context()
+            {
+                Rnd = new(_myKey),
+                Fragment = worldFragment
+            };
+
             /*
              * The center of the fragment relative to the cluster.
              */
-            Vector3 v3Center = _clusterDesc.Pos - worldFragment.Position;
+           Vector3 v3Center = _clusterDesc.Pos - worldFragment.Position;
 
             var listForestQuarters = _clusterDesc.QuarterStore().QueryQuarters(
                 _clusterAABB,
                 Quarter.QuarterAttributes.Forest,
                 Quarter.QuarterAttributes.Forest | Quarter.QuarterAttributes.Building
             );
+
+            foreach (var quarter in listForestQuarters)
+            {
+                _placeNPC(quarter.GetCenterPoint3());
+            }
         };
     }
 
