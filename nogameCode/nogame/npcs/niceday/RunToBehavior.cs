5e492cc97d97764b5dceafaaef7aba8147676c36
Timo Weggen <timo.weggen@gmail.com>
2025-12-31 10:11:33 +0100

    - snap

---
+++ nogameCode/nogame/npcs/niceday/RunToBehavior.cs
@@ -0,0 +1,13 @@
+using nogame.characters;
+
+namespace nogame.npcs.niceday;
+
+
+/**
+ * Behavior to come from a to b on a free field
+ * without checking for collisions
+ */
+public class RunToBehavior
+{
+    public required CharacterModelDescription CharacterModelDescription;
+}
